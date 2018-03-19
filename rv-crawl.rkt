c41725c911ebf2855179141f85cbced41a99e7ee
Lukas Lazarek <LukasALazarek@gmail.com>
2018-03-19 15:33:41 -0400

    Fix page based result comparison

--- rv-crawl.rkt
+++ rv-crawl.rkt
@@ -1,13 +1,17 @@
 #lang racket
 
-(require "./webcrawl.rkt")
+(require "./webcrawl.rkt"
+	 racket/serialize)
+
+(define-serializable-struct entry (title price date url)
+  #:transparent)
 
 (define (in-range?/inclusive low high x)
   (and (>= x low)
        (<= x high)))
 (define (get-price entry)
   (string->number (second (regexp-match "\\$([0-9]+)"
-					(second entry)))))
+					(entry-price entry)))))
 (define (price-within? price-low-limit price-high-limit)
   (λ (entry)
     (in-range?/inclusive price-low-limit
@@ -16,48 +20,67 @@
 
 (define (title-matches? what)
   (λ (entry)
-    (regexp-match? what (first entry))))
+    (regexp-match? what (entry-title entry))))
 
 
 
-(define base-url "https://sarasota.craigslist.org/search/sss?query=rv&\
+(define base-url "https://boston.craigslist.org/search/sss?query=rv&\
 excats=5-15-48-28-1-22-30-13-2-3-5-7-2-9&sort=rel&hasPic=1&searchNearby=1")
 
 (define (find-results base-url)
-  (for/list ([page (range 10)])
-    (define search-url (string-append base-url
-				      "&s="
-				      (number->string (* page 120))))
-    (define search-results (get search-url))
-    (define results-body (fifth search-results))
-    (define results-title&prices
-      (map-matches `(p (@ (class "result-info")) ,_ ...
-		       (time (@ (class "result-date") (datetime ,date) ,_ ...) ,_ ...) ,_ ...
-		       (a (@ (href ,url) ,_ ...) ,title ,_ ...) ,_ ...
-		       (span (@ (class "result-meta")) ,_ ... (span (@ (class "result-price")) ,price) ,_ ...) ,_ ...)
-		   results-body
-		   (list title price url)))
+  (flatten
+   (for/list ([page (range 10)])
+     (define search-url (string-append base-url
+				       "&s="
+				       (number->string (* page 120))))
+     (define search-results (get search-url))
+     (define results-body (fifth search-results))
+     (define results-title&prices
+       (map-matches `(p (@ (class "result-info")) ,_ ...
+			(time (@ (class "result-date") (datetime ,date) ,_ ...) ,_ ...) ,_ ...
+			(a (@ (href ,url) ,_ ...) ,title ,_ ...) ,_ ...
+			(span (@ (class "result-meta")) ,_ ... (span (@ (class "result-price")) ,price) ,_ ...) ,_ ...)
+		    results-body
+		    (make-entry title price date url)))
 
-    (filter (and/c (price-within? 1000 15000)
-		   (or/c (title-matches? "(?i:vr)")
-			 (title-matches? "(?i:trailer)")
-			 (title-matches? "(?i:home)")))
-	    results-title&prices)))
+     (filter result-filter
+	     results-title&prices))))
 
 (define (pretty-print results)
-  (for* ([page results]
-	      [result page])
-    (printf "~a\n~a\n~a\n\n\n" (first result)
-	    (second result)
-	    (third result))))
+  (for ([result results])
+    (printf "~a\n~a\n~a\n~a\n\n\n" (entry-title result)
+	    (entry-price result)
+	    (entry-date result)
+	    (entry-url result))))
 
 (define (load-prev-results)
   (with-handlers ([exn? (λ (e) empty)])
-    (call-with-input-file "seen.dat" read)))
+    (map deserialize (call-with-input-file "seen.dat" read))))
 
 (define (save-results results)
-  (write results (open-output-file "seen.dat" #:exists 'truncate)))
+  (call-with-output-file "seen.dat" (curry write (map serialize results))
+			 #:exists 'truncate))
 
+(define (result=? a b)
+  (and (string=? (entry-title a) (entry-title b))
+       (string=? (entry-price a) (entry-price b))))
+
+(define result-filter
+  (and/c (price-within? 500 15000)
+	 (or/c (title-matches? "(?i:rv)")
+	       (title-matches? "(?i:trailer)")
+	       (title-matches? "(?i:home)")
+	       (title-matches? "(?i:camper)"))
+	 (not/c (or/c (title-matches? "(?i:enclosed)")
+		      (title-matches? "(?i:cargo)")
+		      (title-matches? "(?i:fifth wheel)")
+		      (title-matches? "(?i:5th wheel)")
+		      (title-matches? "(?i:drings)")
+		      (title-matches? "(?i:trade.*for.*your.*rv)")
+		      (title-matches? "(?i:scooter)")
+		      (title-matches? "(?i:dump trailer)")
+		      (title-matches? "(?i:equipment trailer)")
+		      (title-matches? "(?i:rv cam)")))))
 
 
 (printf "Crawling for new results...\n\n")
@@ -65,7 +88,8 @@ excats=5-15-48-28-1-22-30-13-2-3-5-7-2-9&sort=rel&hasPic=1&searchNearby=1")
 (define current-results (find-results base-url))
 (define prev-results (load-prev-results))
 (define new-results (remove* prev-results
-			     current-results))
+			     current-results
+			     result=?))
 (printf "Found these new results:\n\n")
 (pretty-print new-results)
 (printf "\nSaving new results...\n")
